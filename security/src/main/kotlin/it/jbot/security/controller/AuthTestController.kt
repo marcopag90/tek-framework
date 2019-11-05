f5b480841ffeae9f84574a6669dcf88c66ad7f82
Marco <marcopag90@gmail.com>
2019-11-05 12:24:53 +0100

    moved security test controller in security module

--- security/src/main/kotlin/it/jbot/security/controller/AuthTestController.kt
+++ security/src/main/kotlin/it/jbot/security/controller/AuthTestController.kt
@@ -1,4 +1,4 @@
-package it.jbot.application.controller
+package it.jbot.security.controller
 
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.Authentication

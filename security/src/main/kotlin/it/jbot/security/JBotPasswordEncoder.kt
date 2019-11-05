ac472c317c571cc702204795758e6ad8f6b313cf
Marco <marcopag90@gmail.com>
2019-11-05 12:21:34 +0100

    logger and logging optimization of security.properties file

--- security/src/main/kotlin/it/jbot/security/JBotPasswordEncoder.kt
+++ security/src/main/kotlin/it/jbot/security/JBotPasswordEncoder.kt
@@ -3,6 +3,9 @@ package it.jbot.security
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Component
 
+/**
+ * Default JBot Password Encoder {bcript}
+ */
 @Component
 class JBotPasswordEncoder {
     

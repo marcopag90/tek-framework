01e936569aeb3420fa2c4454f981ae5fc2492a44
Marco <marcopag90@gmail.com>
2019-11-16 14:27:12 +0100

    activation profile manager for build purpose

--- security/src/main/kotlin/it/jbot/security/oauth/configuration/ClientDetailsProperties.kt
+++ security/src/main/kotlin/it/jbot/security/oauth/configuration/ClientDetailsProperties.kt
@@ -1,45 +1,22 @@
 package it.jbot.security.oauth.configuration
 
-import it.jbot.shared.util.LoggerDelegate
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.convert.DurationUnit
-import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
-import org.springframework.core.env.Environment
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
 /**
  * Configuration lookup for oauth_client_details table.
  *
- * security.properties file _MUST_ exists with its own profile (eg. security-dev.properties)
- *
- * security properties variables _MUST_ start with _client_ prefix (eg. client.clientId)
+ * Client Properties variables in application.yaml _MUST_ start with _client_ prefix (eg. client.clientId)
  *
  */
 @Configuration
 @ConditionalOnBean(JBotOAuthWebSecurity::class)
-@PropertySource(
-    "classpath:security-\${spring.profiles.active}.properties",
-    ignoreResourceNotFound = true // -> fallback to default values
-)
-@ConfigurationProperties("client")
-class ClientDetailsProperties(
-    environment: Environment,
-    context: ApplicationContext
-) {
-    
-    private val logger by LoggerDelegate()
-    
-    init {
-        
-        if (context.getResource("classpath:security-${environment.activeProfiles[0]}.properties").exists())
-            logger.info("Reading security-${environment.activeProfiles[0]}.properties to evaluate ClientDetailsProperties class")
-        else
-            logger.info("File security-{spring.profiles.active}.properties not found. Using default properties (see ClientDetailsProperties class for further info)")
-    }
+@ConfigurationProperties(prefix = "security.oauth2.client")
+class ClientDetailsProperties {
     
     /**
      * Matching oauth_client_details.client_id to authorize a _jbot client_
@@ -60,6 +37,7 @@ class ClientDetailsProperties(
      * Matching oauth_client_details.authorities to let client inquiry the following paths:
      *
      * _oauth/check_token_
+     *
      * _oauth/token_key_ (if public key is available)
      */
     var authority: String = "ROLE_CLIENT"

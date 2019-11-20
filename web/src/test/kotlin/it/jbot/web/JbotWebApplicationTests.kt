32437b9715297fefd7603adc881fa8183b6385c0
Marco <marcopag90@gmail.com>
2019-11-20 20:41:45 +0100

    project module refactoring (added web module..to do movements)

---
+++ web/src/test/kotlin/it/jbot/web/JbotWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.jbot.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JbotWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

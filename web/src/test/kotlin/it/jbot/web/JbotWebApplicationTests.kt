03319a3592b7a99d0cfd3a3ec835c78ba90aa45c
Marco <marcopag90@gmail.com>
2019-11-20 20:49:14 +0100

    changed test configuration for web module

--- web/src/test/kotlin/it/jbot/web/JbotWebApplicationTests.kt
+++ web/src/test/kotlin/it/jbot/web/JbotWebApplicationTests.kt
@@ -1,9 +1,11 @@
 package it.jbot.web
 
 import org.junit.jupiter.api.Test
+import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 
-@SpringBootTest
+@RunWith(SpringRunner::class)
 class JbotWebApplicationTests {
 
 	@Test

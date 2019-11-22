292becdede05c32a8f6e7b4a278b71cf7f07b9b3
Marco <marcopag90@gmail.com>
2019-11-22 20:06:50 +0100

    fix AuthPort annotation

--- security/src/main/kotlin/it/jbot/security/port/AuthPort.kt
+++ security/src/main/kotlin/it/jbot/security/port/AuthPort.kt
@@ -5,10 +5,11 @@ import it.jbot.security.dto.RegisterForm
 import it.jbot.web.JBotResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 interface AuthPort {
 
     @PostMapping(SecurityConstant.REGISTER_PATTERN)
-    fun register(registerForm: RegisterForm) : ResponseEntity<JBotResponse>
+    fun register(@RequestBody registerForm: RegisterForm): ResponseEntity<JBotResponse>
 
 }

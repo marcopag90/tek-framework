95ac2387d7f9154290621ea7f3e1a81bf006c391
Marco <marcopag90@gmail.com>
2019-12-09 19:55:46 +0100

    changed not acceptable role not found

--- security/src/main/kotlin/it/jbot/security/model/enums/RoleName.kt
+++ security/src/main/kotlin/it/jbot/security/model/enums/RoleName.kt
@@ -1,37 +1,37 @@
 package it.jbot.security.model.enums
 
-import it.jbot.security.i18n.SecurityMessageSource
-import it.jbot.security.i18n.SecurityMessageSource.Companion.errorRoleNotFound
 import it.jbot.core.exception.JBotServiceException
 import it.jbot.core.exception.ServiceExceptionData
 import it.jbot.core.util.LabelEnum
+import it.jbot.security.i18n.SecurityMessageSource
+import it.jbot.security.i18n.SecurityMessageSource.Companion.errorRoleNotFound
 import org.springframework.http.HttpStatus
 
 enum class RoleName(
     override val label: String
 ) : LabelEnum {
-    
+
     ROLE_ADMIN("Administrator"),
     ROLE_USER("User");
-    
+
     companion object {
-        
+
         fun fromString(name: String): RoleName {
-            
+
             val messageSource = SecurityMessageSource()
-            
+
             return when (name) {
-                
+
                 ROLE_ADMIN.name -> ROLE_ADMIN
                 ROLE_USER.name -> ROLE_USER
-                
+
                 else -> throw JBotServiceException(
                     data = ServiceExceptionData(
                         source = messageSource,
                         message = errorRoleNotFound,
                         parameters = arrayOf(name)
                     ),
-                    httpStatus = HttpStatus.NOT_FOUND
+                    httpStatus = HttpStatus.NOT_ACCEPTABLE
                 )
             }
         }

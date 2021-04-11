aafc7ddab3c99e03ca0352850898aee28860e04d
Marco <marcopag90@gmail.com>
2021-04-11 13:12:42 +0200

    spring filter dependency
    spring filter dependency

--- core/src/test/java/com/tek/core/rsql/RsqlApplication.java
+++
@@ -1,8 +0,0 @@
-package com.tek.core.rsql;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "com.tek.core.rsql")
-public class RsqlApplication {
-
-}
--- core/src/test/java/com/tek/core/rsql/repository/DeveloperRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tek.core.rsql.repository;
-
-import com.tek.core.rsql.model.Developer;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DeveloperRepository
-    extends CrudRepository<Developer, Integer>, JpaSpecificationExecutor<Developer> {
-}

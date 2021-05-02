92b18c6222d8a7cd7fa42b27a702396d60eebf67
Marco <marcopag90@gmail.com>
2021-05-02 21:42:46 +0200

    test injection success

--- tek-core/src/main/java/com/tek/core/TekCoreAutoConfig.java
+++ tek-core/src/main/java/com/tek/core/TekCoreAutoConfig.java
@@ -4,7 +4,6 @@ import com.tek.core.constants.TekCoreConstants;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//TODO test if this works
 @Configuration
 @ComponentScan(TekCoreConstants.TEK_CORE_PACKAGES_TO_SCAN)
 public class TekCoreAutoConfig {

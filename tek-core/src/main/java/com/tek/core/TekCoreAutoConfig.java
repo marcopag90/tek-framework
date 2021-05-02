082330b3a1616ce3819941efb04718fd740409ae
marco <marcopag90@gmail.com>
2021-05-02 15:25:55 +0200

    added todo

--- tek-core/src/main/java/com/tek/core/TekCoreAutoConfig.java
+++ tek-core/src/main/java/com/tek/core/TekCoreAutoConfig.java
@@ -4,6 +4,7 @@ import com.tek.core.constants.TekCoreConstants;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+//TODO test if this works
 @Configuration
 @ComponentScan(TekCoreConstants.TEK_CORE_PACKAGES_TO_SCAN)
 public class TekCoreAutoConfig {

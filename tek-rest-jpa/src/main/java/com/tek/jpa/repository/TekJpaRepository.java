951320c2a387a4c85f386ca603a8f0d61aec80b6
Marco <marcopag90@gmail.com>
2021-05-16 16:37:26 +0200

    code refactoring

--- tek-rest-jpa/src/main/java/com/tek/jpa/repository/TekJpaRepository.java
+++ tek-rest-jpa/src/main/java/com/tek/jpa/repository/TekJpaRepository.java
@@ -1,19 +1,9 @@
 package com.tek.jpa.repository;
 
-import java.util.Optional;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface TekJpaRepository<T, I> extends CrudRepository<T, I>, JpaSpecificationExecutor<T> {
-
-  @Override
-  Page<T> findAll(Specification<T> specification, Pageable pageable);
-
-  @Override
-  Optional<T> findById(I i);
 }
